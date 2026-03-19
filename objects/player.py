f15a1bf4dd74a3253662692c2e6849fd5d653a02
PedroLino <ph583152@gmail.com>
2026-03-18 21:24:22 -0300

    jogador atira no espaco

--- objects/player.py
+++ objects/player.py
@@ -1,7 +1,9 @@
 import pygame
 from objects.game_object import GameObject
+from objects.projectile import Projectile
 
 class Player(GameObject):
+    
     def __init__(self, x, y, width=50, height=50, speed=5, jump_strength=-10, gravity=0.5):
         super().__init__(x, y)
         self.width = width
@@ -12,11 +14,17 @@ class Player(GameObject):
         self.jump_strength = jump_strength
         self.jumping = False
 
+        self.direction = 1  # 1 = direita, -1 = esquerda
+        self.shoot_delay = 500
+        self.last_shot = pygame.time.get_ticks()
+
     def handle_input(self, keys):
         if keys[pygame.K_a]:
             self.x -= self.speed
+            self.direction = -1
         if keys[pygame.K_d]:
             self.x += self.speed
+            self.direction = 1
         if keys[pygame.K_w] and not self.jumping:
             self.vel_y = self.jump_strength
             self.jumping = True
@@ -43,3 +51,15 @@ class Player(GameObject):
 
     def draw(self, screen):
         pygame.draw.rect(screen, (255, 0, 0), (self.x, self.y, self.width, self.height))
+
+    def shoot(self, projectiles_list):
+        now = pygame.time.get_ticks()
+        if now - self.last_shot > self.shoot_delay:
+            from objects.projectile import Projectile
+            proj = Projectile(
+                self.x + self.width // 2,
+                self.y + self.height // 2,
+                self.direction
+            )
+            projectiles_list.append(proj)
+            self.last_shot = now
--- scenes/GameWorld.py
+++ scenes/GameWorld.py
@@ -10,7 +10,8 @@ class GameWorld(GameScene):
         self.font = pygame.font.SysFont("Arial", 40)
         self.ground_y = 600
 
-        self.projectiles = []
+        self.player_projectiles = []
+        self.enemy_projectiles = []
        
         self.platforms = [
             pygame.Rect(150, 220, 200, 10), 
@@ -39,30 +40,38 @@ class GameWorld(GameScene):
     def update(self):
         keys = pygame.key.get_pressed()
         self.player.handle_input(keys)
+
         self.player.update(self.platforms, self.ground_y)
 
+        if keys[pygame.K_SPACE]:
+            self.player.shoot(self.player_projectiles)
 
+        # inimigos
         for enemy in self.enemies:
-            enemy.update(self.platforms, self.ground_y, self.projectiles)
+            enemy.update(self.platforms, self.ground_y, self.enemy_projectiles)
 
-        
-        for proj in self.projectiles[:]:
+        # tiros dos inimigos
+        for proj in self.enemy_projectiles[:]:
             proj.update()
-            if proj.x < 0 or proj.x > 1080: 
-                self.projectiles.remove(proj)
+            if proj.x < 0 or proj.x > 1080:
+                self.enemy_projectiles.remove(proj)
 
-       
+        # tiros do player
+        for proj in self.player_projectiles[:]:
+            proj.update()
+
+            for enemy in self.enemies[:]:
+                enemy_rect = pygame.Rect(enemy.x, enemy.y, enemy.width, enemy.height)
+                if proj.get_rect().colliderect(enemy_rect):
+                    self.enemies.remove(enemy)
+                    self.player_projectiles.remove(proj)
+                    break
+
+        # colisão com player
         player_rect = pygame.Rect(self.player.x, self.player.y, self.player.width, self.player.height)
-        for proj in self.projectiles:
+
+        for proj in self.enemy_projectiles:
             if proj.get_rect().colliderect(player_rect):
-                from scenes.death_menu import DeathMenu
-                self.next_scene = DeathMenu()
-
-        player_rect = pygame.Rect(self.player.x, self.player.y, self.player.width, self.player.height)
-
-        for enemy in self.enemies:
-            enemy_rect = pygame.Rect(enemy.x, enemy.y, enemy.width, enemy.height)
-            if player_rect.colliderect(enemy_rect):
                 from scenes.death_menu import DeathMenu
                 self.next_scene = DeathMenu()
 
@@ -80,5 +89,8 @@ class GameWorld(GameScene):
         text = self.font.render("escale", True, (255, 255, 255))
         screen.blit(text, (350, 50))
 
-        for proj in self.projectiles:
+        for proj in self.player_projectiles:
+            proj.draw(screen)
+
+        for proj in self.enemy_projectiles:
             proj.draw(screen)
